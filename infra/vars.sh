3129542a15a2140eb504a83b39dfe7d315a5f284
Manasa Kandula <manasakandula.m@gmail.com>
2024-01-31 20:11:55 +0000

    added gateway build yaml

--- infra/vars.sh
+++ infra/vars.sh
@@ -11,27 +11,17 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# KCC
-export KCC_REGION=us-central1
+
 # Prod clusters
-export GKE_PROD_REGION_1=us-central1
-export GKE_PROD_REGION_2=us-west2
-export GKE_PROD1_NAME=prod-us-central1-0
-export GKE_PROD1_LOCATION=us-central1-a
-export GKE_PROD2_NAME=prod-us-central1-1
-export GKE_PROD2_LOCATION=us-central1-b
-export GKE_PROD3_NAME=prod-us-central1-2
-export GKE_PROD3_LOCATION=us-central1-c
-export GKE_PROD4_NAME=prod-us-west2-0
-export GKE_PROD4_LOCATION=us-west2-a
-export GKE_PROD5_NAME=prod-us-west2-1
-export GKE_PROD5_LOCATION=us-west2-b
-export GKE_PROD6_NAME=prod-us-west2-2
-export GKE_PROD6_LOCATION=us-west2-c
+export GKE_PROD_REGION_1=europe-west3-0
+export GKE_PROD_REGION_2=europe-west3
+export GKE_PROD1_NAME=europe-west3-0
+export GKE_PROD1_LOCATION=europe-west4-0
+export GKE_PROD2_NAME=europe-west4
+export GKE_PROD2_LOCATION=europe-west4-0
 # GKE Config
-export GKE_CONFIG_NAME=config-us-central1
-export GKE_CONFIG_REGION=us-central1
-export GKE_CONFIG_LOCATION=us-central1-a
+export GKE_CONFIG_NAME=config-europe-west4
+export GKE_CONFIG_REGION=europe-west4
 
 
 
